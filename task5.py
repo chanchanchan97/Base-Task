0dec8abe74b250918856be1075c692aadd971871
chanchanchan97 <39607836+chanchanchan97@users.noreply.github.com>
2021-06-08 01:04:52 +0800

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+list = []
+for i in range(5):
+    list.append(i)
+print('list:', list)
+
+dict = {'A': 1, 'B': 2, 'C': 3}
+print('dict:', dict)
+print(dict['A'])
